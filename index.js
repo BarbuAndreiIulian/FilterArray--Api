c171daf8af786abf8a937c5c534dff8ed857577f
BarbuAndreiIulian <83908356+BarbuAndreiIulian@users.noreply.github.com>
2022-02-09 23:05:07 +0100

    exercise
    exercise

---
+++ index.js
@@ -0,0 +1,26 @@
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick);
+
+const people = [
+  { name: "Jack", hasPet: true },
+  { name: "Jill", hasPet: false },
+  { name: "Alice", hasPet: true },
+  { name: "Bob", hasPet: false },
+];
+
+function filterArray(array, callback) {
+  const resultingArray = [];
+  // Write your filtering logic here
+  for (let item of array) {
+    const shouldBeIncluded = callback(item);
+    if (shouldBeIncluded) {
+      resultingArray.push(item);
+    }
+  }
+  return resultingArray;
+}
